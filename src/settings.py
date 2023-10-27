21c147f5c180c87645a2791f49ce749b16d7326d
erastov-alex <erastov.alex@gmail.com>
2023-10-27 18:11:40 +0300

    unhide

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Traffic Vehicles Object Detection"
 PROJECT_NAME_FULL: str = "Traffic Vehicles Object Detection"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
